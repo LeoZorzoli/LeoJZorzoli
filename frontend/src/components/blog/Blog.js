18498f13f9a00b30fb1685905668144425c08c1d
LeoZorzoli <zorzolileonel@gmail.com>
2020-12-04 11:32:45 -0300

    update style

--- frontend/src/components/blog/Blog.js
+++ frontend/src/components/blog/Blog.js
@@ -5,7 +5,7 @@ const Blog = (props) => {
     return(
         <div className="col-md-12 col-sm-12 col-lg-4 blog">
             <img src={props.img} alt={props.name} className="img-project img-fluid img-responsive rounded" />
-            <div className="blog-info">
+            <div className="blog-info rounded">
                 <p className="blogReadTime">{props.read} read - Date {props.date} - <a href={props.src} className="linkBlog" target="_blank" rel="noopener noreferrer">Link</a></p>
                 <p className="blogTitle">{props.name}</p>
                 <p className="blogDescription">{props.description}</p>
